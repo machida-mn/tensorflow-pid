f4a0faaeda18c22e5e30a21409cdbddf5abaff58
machida-mn <machida_mn@complex.ist.hokudai.ac.jp>
2018-07-25 12:41:55 +0900

    Add url and license in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ from setuptools import find_packages
 setup(name='tensorflow-pid',
       version='1.0.0',
       description='Tensorflow implementation of PID optimization',
+      url='https://github.com/machida-mn/tensorflow-pid',
       author='Ryohei Machida',
       author_email='machida_mn@complex.ist.hokudai.ac.jp',
+      license='MIT',
       packages=find_packages(),
       install_requires=[
           'tensorflow'
